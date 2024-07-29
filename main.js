8497b049c65113ec9d12c3b57575ee0c69bd1063
meataxe <mark.bellhouse@gmail.com>
2024-07-30 11:35:17 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 function init () {
   console.log("init");
+
+  document.getElementById("input-f5").addEventListener("onInput", function (e) { showCurrentValue(e, "f5"); });  
+  document.getElementById("input-rdp").addEventListener("onInput", function (e) { showCurrentValue(e, "rdp"); });  
+}
+
+function showCurrentValue(event, sourceData)
+{
+    console.log("showCurrentValue: " + sourceData);
+  
+    const value = event.target.value;
+    document.getElementById("label").innerText = value;
+
+    console.log(this.className); // logs the className of my_element
+    console.log(e.currentTarget === this); // logs `true`
 }
 
 window.load(function() { init(); });
