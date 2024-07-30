081fdbc5b1f7155511b5f7e8b02ef1ee47b4997e
meataxe <mark.bellhouse@gmail.com>
2024-07-30 12:18:03 +1200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var f5 = `H9MKYDQ099
 Y6491TXY0J
 80M13EFT1Y
 FHK4J9YQRP
-NT37RKRJ7E`;
+NT37RKRJ7E`.split("\n");
 
 var rdp = `TC8582K2MD 
 M628K6KH6V
 XY7JFR31XM
 YWYWHMHEXN
-TYMVPDQ2J2`;
+TYMVPDQ2J2`.split("\n");
 
 function init () {
   console.log("init");
@@ -19,14 +19,27 @@ function init () {
 
 function showCurrentValue(event)
 {
-    console.log("showCurrentValue");
-  
+    console.log("showCurrentValue");  
     const value = event.target.value;
-    document.getElementById("label").innerText = value;
+    const src = this.id.split('-')[1];
 
+    if (value.length !== 6 || !["rdp", "f5"].includes(value) ) { return; }
+
+    /* [item][index][row|column] */
+    const offset = "a".charCodeAt(0);
+    var 
+      i1c = value[0].charCodeAt(0) - offset, i1r = value[1],
+      i2c = value[2].charCodeAt(0) - offset, i2r = value[3],
+      i3c = value[4].charCodeAt(0) - offset, i3r = value[5];
+
+    var result = src === "f5"
+      ? f5[i1r][i1c] + f5[i2r][i2c] + f5[i2r][i2c]
+      : rdp[i1r][i1c] + rdp[i2r][i2c] + rdp[i2r][i2c];
+  
     console.log(this.className); // logs the className of my_element
     console.log(e.currentTarget === this); // logs `true`
     console.log("value: " + value);
+    console.log("result: " + result);
 }
 
 document.addEventListener("DOMContentLoaded", function(arg) {
